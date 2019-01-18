2f818b72e8973d7db46a0020dd8a5fbcab70a5f9
golfzaza0512 <golfzaza051255@hotmail.com>
2019-01-18 13:56:55 +0700

    K U Y

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 
+// const express = require('express');
+// const app = express();
+// app.use(express.static(__dirname + '/dist'));
+// app.listem(process.env.PORT || 8080);
+
+
+//Install express server
 const express = require('express');
+const path = require('path');
+ 
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listem(process.env.PORT || 8080);
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/my-profile'
+app.use(express.static(__dirname + '/dist/my-profile'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/my-profile/index.html'
+  res.sendFile(path.join(__dirname + '/dist/my-profile/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
