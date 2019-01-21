b14bb09ba9e2cf1c0e3c8a984f96caa3acd3157d
golfzaza0512 <golfzaza051255@hotmail.com>
2019-01-21 16:07:36 +0700

    add navbra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainProfileComponent } from './main-profile/main-profile.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainProfileComponent,
+    SocialMediaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/profile', pathMatch: 'full' },
+      { path: 'profile', component: MainProfileComponent },
+      { path: 'socisl', component: SocialMediaComponent }
+      // , { path: 'about', loadChildren: '../about/about.module#AboutModule' }
+      // , { path: 'contact', loadChildren: '../about/about.module#AboutModule' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-profile/main-profile.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-profile',
+  templateUrl: './main-profile.component.html',
+  styleUrls: ['./main-profile.component.css']
+})
+export class MainProfileComponent implements OnInit {
+  title = 'PROFILE';
+  name = 'Weerapol Sanpradaem';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
